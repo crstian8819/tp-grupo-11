a4931b283362cd9524db144bef882f5638cd2ed0
Cristian Sosa <cristiansosa8819@gmail.com>
2026-09-13 22:17:15 -0300

    Agregacion del bootsrap al repositorio

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+class Persona {
+    nombre;
+    apellido;
+    constructor() {
+        this.nombre = "";
+        this.apellido = "";
+    }
+}
+const cris = new Persona();
+cris.nombre = "Hola";
+cris.apellido = " Kc";
+``;
+console.log(`################`);
+console.log(`Nombre: ${cris.nombre}`);
+console.log(`Apellido: ${cris.apellido}`);
+console.log(`################`);
+//# sourceMappingURL=index.js.map
---
+++ dist/persona.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=persona.js.map
